43cf640f69064a49326195c465647dfd05d56e46
mohamed-h-rabie <mohamed.hassa3nnn@gmail.com>
2025-04-24 12:45:15 +0200

    Handel Post request and respond to URL parameters

--- app.js
+++ app.js
@@ -1,18 +1,53 @@
 const express = require('express');
 const fs = require('fs');
+const { url } = require('inspector');
 const app = express();
+app.use(express.json());
 const port = 3000;
-
+const toursRoute = '/api/v1/tours';
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+app.get(toursRoute, (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
-    tours: tours,
+    data: { tours },
   });
+});
+app.get(`${toursRoute}/:id`, (req, res) => {
+  const id = req.params.id;
+  const tour = tours.find((tour) => +tour.id === +id);
+  if (!tour) {
+   return  res.status(404).json({
+      status: 'failed',
+      message: 'Invailed ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+app.post(toursRoute, (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 // app.get('/', (req, res) => {
